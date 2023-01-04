54a79346aaade15c4dd589c613b176751c29d0e2
Pius Prince <45078705+piusprince@users.noreply.github.com>
2023-01-04 23:37:59 +0000

    Initial commit

---
+++ Base64Encoding/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Allocate array of 128 bytes
+byte[] binaryObject = new byte[128];
+
+// Fill array with random bytes
+Random.Shared.NextBytes(binaryObject);
+Console.WriteLine("The binary object as bytes: ");
+for (int index = 0; index < binaryObject.Length; index++)
+{
+    Console.Write($"{binaryObject[index]:X} ");
+}
+Console.WriteLine();
+
+// Convert to base64 string 
+string encoded = Convert.ToBase64String(binaryObject);
+Console.WriteLine($"Binary object as base64 encoded: {encoded}");
